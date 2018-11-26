43e6c51d76e12ee264c6c29b10f6798691e4f415
barmescr <barmescr@rose-hulman.edu>
2018-11-26 14:57:41 -0500

    DONE

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -92,7 +92,7 @@ print(2**0.5)
 # After you have written and run the code to learn the TYPE
 # of each of the above, change the above _TODO_ to DONE.
 ###############################################################################
-
+print(type(hello))
 ###############################################################################
 #
 # TODO: 6.
